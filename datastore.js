a278642fcc8de0666e440e7f564c684c6cdb7e47
Patrick Dicker <mr.patrick.dicker@gmail.com>
2019-02-16 22:12:42 -0700

    wireframe complete

---
+++ datastore.js
@@ -0,0 +1,97 @@
+const STORE = [
+    {
+      question: 'Who are the Bogdanoffs?',
+      answers: ["The Alpha", "The Omega",
+        "The puppetmasters",
+        "French twin brothers who are television presenters"
+        ],
+      correctAnswer: 'French twin brothers who are television presenters',
+    },
+    {
+        question: 'Why are the Bogs famous?',
+        answers: ["They're the ringleaders of a central cabal, which controls all of the world's monetary supply.",
+        "They look unusual.",
+        "They are the architects of the matrix we currently inhabit",
+        "They are intraterrestrial time-travelers"],
+        correctAnswer: "They look unusual.",
+      },
+
+
+      {
+        question: 'What online community are the Bogdanoff twins most prominently associated with?',
+        answers: ["Club Penguin",
+            'My Little Pony imageboards',
+            'Cryptocurrency',
+            'Backpage'],
+        correctAnswer: "Cryptocurrency",
+      },
+
+     {
+         question:  "Dr. Bogdanoff & Dr. Bogdanoff are most renown in the scientific community for their accomplishments in...",
+        answers: ["Publishing nonsensical theories",
+            "Disproving the Theory of Relativity",
+            "Zoology",
+            "prosthetics"
+        ],
+        correctAnswer:"Publishing nonsensical theories" ,
+        },
+
+        {
+        question:  "How many children do the Bogdanoff brothers have?",
+        answers: ["We are all Bogs children.",
+            "6",
+            "9",
+            "The answer is unfamthomable, mind boggling."
+        ],
+        correctAnswer:"6" ,
+        },
+
+        {
+        question:  "What disorder is it theorized the twins suffer from?",
+        answers: [ "Acromegaly",
+            "Telepathy",
+            "Greed",
+            "Vainity",
+        ],
+        correctAnswer:"Acromegaly" ,
+        },
+
+        {
+        question:  "The Bog boys were raised by...",
+        answers: ["An AI sent from the future to divine its chosen followers",
+            "Extraterrestrials",
+            "Cory Baxter",
+            "Countess Bertha Kolowrat-Krakowská" ],
+        correctAnswer: "Countess Bertha Kolowrat-Krakowská",
+        },
+
+        {
+        question:  "What is the Bogdanoff's one true weakness?",
+        answers: ["Nobody",
+            "No one",
+            "Charles Norris",
+            "Absolutely nothing, not even Chuck Norris"
+        ],
+        correctAnswer:"Absolutely nothing, not even Chuck Norris" ,
+        },
+
+        {
+        question:  "Who would win in a fight, Igor Bogdanoff or Grichka Bogdanoff?",
+        answers: ["They would both win, we would all lose.",
+            "Igor",
+            "Grichka",
+            "Ron Paul"
+        ],
+        correctAnswer:"They would both win, we would all lose." ,
+        },
+
+        {
+        question:  "How do the Bogdanoffs pronounce 'meme'?",
+        answers: ["may may",
+        "rhymes with dream",
+        "rhymes with cream",
+        "rhymes with Kanye's hit song 'Ultralight Beam'"
+        ],
+        correctAnswer:"may may" ,
+        }
+    ]
